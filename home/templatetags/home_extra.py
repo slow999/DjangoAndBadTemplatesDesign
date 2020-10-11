3ca5979aed9e3803e790a1e531ef174b6884a147
slow999 <slow999@gmail.com>
2020-10-11 12:38:20 -0500

    remove template tag

--- home/templatetags/home_extra.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-
-register = template.Library()
-
-
-@register.inclusion_tag('sidebar.html')
-def get_home_sidebar(request, perms=None):
-    return {'request': request, 'perms': perms}
